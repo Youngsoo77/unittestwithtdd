cc1383819ab1b2f983eef7986b8b2176a9c33ce0
youngso0 <youngsoo@invesume.com>
2017-04-05 16:04:49 +0900

    coverage test 커버율 떨어뜨리기

--- lists/tests.py
+++ lists/tests.py
@@ -9,31 +9,31 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        # resolve 는 django 가 내부적으로 사용하는 함수로 URL을 해석해서 일치하는 뷰 함수를 찾는다.
-        self.assertEqual(found.func, home_page)
+    # def test_root_url_resolves_to_home_page_view(self):
+    #     found = resolve('/')
+    #     # resolve 는 django 가 내부적으로 사용하는 함수로 URL을 해석해서 일치하는 뷰 함수를 찾는다.
+    #     self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+    # def test_home_page_returns_correct_html(self):
+    #     request = HttpRequest()
+    #     response = home_page(request)
+    #     expected_html = render_to_string('home.html')
+    #     self.assertEqual(response.content.decode(), expected_html)
 
-        # self.assertTrue(response.content.startswith(b'<html>'))
-        # self.assertIn(b'<title>Hello world Unittest with TDD</title>', response.content)
-        # self.assertTrue(response.content.strip().endswith(b'</html>'))
+    #     # self.assertTrue(response.content.startswith(b'<html>'))
+    #     # self.assertIn(b'<title>Hello world Unittest with TDD</title>', response.content)
+    #     # self.assertTrue(response.content.strip().endswith(b'</html>'))
 
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = '신규 작업 아이템'
+    # def test_home_page_can_save_a_POST_request(self):
+    #     request = HttpRequest()
+    #     request.method = 'POST'
+    #     request.POST['item_text'] = '신규 작업 아이템'
 
-        response = home_page(request)
+    #     response = home_page(request)
 
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': '신규 작업 아이템'}
-        )
-        self.assertEqual(response.content.decode(), expected_html)
+    #     self.assertIn('신규 작업 아이템', response.content.decode())
+    #     expected_html = render_to_string(
+    #         'home.html',
+    #         {'new_item_text': '신규 작업 아이템'}
+    #     )
+    #     self.assertEqual(response.content.decode(), expected_html)
