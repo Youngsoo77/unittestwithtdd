1f9f709961e46f1f037de844c566033ae8b87703
youngso0 <youngsoo@invesume.com>
2017-04-05 14:22:32 +0900

    기능테스트가 작업 아이템을 추가 할 수 있는지 확인

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -13,7 +14,26 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do', self.browser.title)
+        self.assertIn('TDD', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Hello', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
+        # 공작 깃털 사기 라고 텍스트 상자에 입력한다.
+        inputbox.send_keys('공작 깃털 사기')
+
+        # 엔터키를 치면 페이지가 갱신되고 작업 목록에 추가된다.
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작 깃털 사기' for row in rows),
+        )
         self.fail('Finish the Test!')
 
 
